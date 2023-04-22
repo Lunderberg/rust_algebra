f200a605737691b45e01eb950f2149c1a3f142c3
Eldritch Cheese <eldritch.cheese@gmail.com>
2023-04-22 09:01:21 -0500

    Updated PartialEq to remove wildcard match
    When matching on `(self, other)`, there's a wildcard `_ => false` to
    catch mismatched types.  This wildcard also makes the match
    exhaustive, even if a new node type has been implemented.  Rewriting
    to use `match self`, with `matches!(other, ...)` in the value, so that
    the addition of new operations causes a compilation error, and
    requiring these implementations to be updated.

--- src/expr_equality.rs
+++ src/expr_equality.rs
@@ -5,80 +5,80 @@ use typed_dag::Visitable;
 
 impl<'view, V: expr::visitor::Expr<'view>> PartialEq for expr::Expr<V> {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Int(l0), Self::Int(r0)) => l0.borrow() == r0.borrow(),
-            (Self::Bool(l0), Self::Bool(r0)) => l0.borrow() == r0.borrow(),
-            (Self::Rational(l0), Self::Rational(r0)) => l0.borrow() == r0.borrow(),
-            _ => false,
+        match self {
+            Self::Int(l0) => matches!(other, Self::Int(r0) if l0.borrow() == r0.borrow()),
+            Self::Bool(l0) => matches!(other,  Self::Bool(r0) if l0.borrow() == r0.borrow()),
+            Self::Rational(l0) => matches!(other, Self::Rational(r0)if l0.borrow() == r0.borrow()),
         }
     }
 }
 impl<'view, V: expr::visitor::Expr<'view>> PartialEq for expr::Int<V> {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Literal(l0), Self::Literal(r0)) => l0 == r0,
-            (Self::Add(l0, l1), Self::Add(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+        match self {
+            Self::Literal(l0) => matches!(other, Self::Literal(r0) if l0 == r0),
+            Self::Add(l0, l1) => {
+                matches!(other, Self::Add(r0,r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::Sub(l0, l1), Self::Sub(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Sub(l0, l1) => {
+                matches!(other, Self::Sub(r0,r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::Mul(l0, l1), Self::Mul(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Mul(l0, l1) => {
+                matches!(other, Self::Mul(r0,r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::FloorDiv(l0, l1), Self::FloorDiv(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::FloorDiv(l0, l1) => {
+                matches!(other, Self::FloorDiv(r0,r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::FloorMod(l0, l1), Self::FloorMod(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::FloorMod(l0, l1) => {
+                matches!(other, Self::FloorMod(r0,r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::Negative(l0), Self::Negative(r0)) => l0.borrow() == r0.borrow(),
-            _ => false,
+            Self::Negative(l0) => matches!(other, Self::Negative(r0) if l0.borrow() == r0.borrow()),
         }
     }
 }
 impl<'view, V: expr::visitor::Expr<'view>> PartialEq for expr::Bool<V> {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Literal(l0), Self::Literal(r0)) => l0 == r0,
-            (Self::IntEqual(l0, l1), Self::IntEqual(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+        match self {
+            Self::Literal(l0) => matches!(other, Self::Literal(r0) if l0 == r0),
+            Self::IntEqual(l0, l1) => matches!(other, Self::IntEqual(r0, r1) if
+                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()),
+
+            Self::RationalEqual(l0, l1) => matches!(other, Self::RationalEqual(r0, r1) if
+                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()),
+
+            Self::Not(l0) => matches!(other, Self::Not(r0) if l0.borrow() == r0.borrow()),
+            Self::And(l0, l1) => {
+                matches!(other, Self::And(r0, r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::RationalEqual(l0, l1), Self::RationalEqual(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Or(l0, l1) => {
+                matches!(other, Self::Or(r0, r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow())
             }
-            (Self::Not(l0), Self::Not(r0)) => l0.borrow() == r0.borrow(),
-            (Self::And(l0, l1), Self::And(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
-            }
-            (Self::Or(l0, l1), Self::Or(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
-            }
-            _ => false,
         }
     }
 }
 impl<'view, V: expr::visitor::Expr<'view>> PartialEq for expr::Rational<V> {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Int(l0), Self::Int(r0)) => l0.borrow() == r0.borrow(),
-            (Self::Ratio(l0, l1), Self::Ratio(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+        match self {
+            Self::Int(l0) => {
+                matches!(other, Self::Int(r0)       if l0.borrow() == r0.borrow                               ())
             }
-            (Self::Negative(l0), Self::Negative(r0)) => l0.borrow() == r0.borrow(),
-            (Self::Add(l0, l1), Self::Add(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Ratio(l0, l1) => {
+                matches!(other, Self::Ratio(r0, r1) if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow ())
             }
-            (Self::Sub(l0, l1), Self::Sub(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Negative(l0) => {
+                matches!(other, Self::Negative(r0)  if l0.borrow() == r0.borrow                               ())
             }
-            (Self::Mul(l0, l1), Self::Mul(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Add(l0, l1) => {
+                matches!(other, Self::Add(r0, r1)   if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow ())
             }
-            (Self::Div(l0, l1), Self::Div(r0, r1)) => {
-                l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow()
+            Self::Sub(l0, l1) => {
+                matches!(other, Self::Sub(r0, r1)   if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow ())
             }
-            _ => false,
+            Self::Mul(l0, l1) => {
+                matches!(other, Self::Mul(r0, r1)   if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow ())
+            }
+            Self::Div(l0, l1) => {
+                matches!(other, Self::Div(r0, r1)   if l0.borrow() == r0.borrow() && l1.borrow() == r1.borrow ())
+            }
         }
     }
 }
